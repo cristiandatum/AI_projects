3cae3eb41fa71ac8d9a1bc84207c68bc90a026ec
Cristian Alberch <cristian.alberch@outlook.com>
2020-05-05 07:54:33 +0300

    completed air_cargo problems

--- 2_Classical Planning/run_search_combo.py
+++ 2_Classical Planning/run_search_combo.py
@@ -1,4 +1,3 @@
-
 import sys 
 import run_search
 import time
@@ -7,8 +6,8 @@ import subprocess
 
 file_results=open('report_output.txt','w')
 
-for p in range(1,5,1):
-    for h in range (1,12,1):
+for p in range(1,5,1): #problems 1 to 4
+    for s in range (1,12,1): #heuristics 1 to 11
 
         run_search_dir='python run_search.py'+ ' -p '+ str(p)+ ' -s '+str(s)
 
