54090c094a2bf997c3f9491fb0c64321c876f239
Cristian Alberch <cristian.alberch@outlook.com>
2020-05-19 10:14:06 +0300

    minimax without pruning

--- 3_Adversarial Search/my_custom_player.py
+++ 3_Adversarial Search/my_custom_player.py
@@ -1,22 +1,6 @@
-
-import pickle
 from isolation import Isolation
 from sample_players import DataPlayer
-
-for play in range(10):
-    state = Isolation()
-    my_data = {state: 57}  # opening book always chooses the middle square on an open board
-    pickle_out = open('data.pickle', 'wb')
-    pickle.dump(my_data,pickle_out)
-    
-pickle_out.close()
-
-
-pickle_in=open('data.pickle','rb')
-example=pickle.load(pickle_in)    
-
-print(example)
-print(DataPlayer)
+import random
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -36,47 +20,50 @@ class CustomPlayer(DataPlayer):
     **********************************************************************
     """
     def get_action(self, state):
-        """ Employ an adversarial search technique to choose an action
-        available in the current state calls self.queue.put(ACTION) at least
+        """ Choose an action available in the current state
+
+        See RandomPlayer and GreedyPlayer for examples.
 
         This method must call self.queue.put(ACTION) at least once, and may
-        call it as many times as you want; the caller will be responsible
-        for cutting off the function after the search time limit has expired.
-
-        See RandomPlayer and GreedyPlayer in sample_players for more examples.
+        call it as many times as you want; the caller is responsible for
+        cutting off the function after the search time limit has expired. 
 
         **********************************************************************
-        NOTE: 
-        - The caller is responsible for cutting off search, so calling
-          get_action() from your own code will create an infinite loop!
-          Refer to (and use!) the Isolation.play() function to run games.
+        NOTE: since the caller is responsible for cutting off search, calling
+              get_action() from your own code will create an infinite loop!
+              See (and use!) the Isolation.play() function to run games.
         **********************************************************************
         """
-        # TODO: Replace the example implementation below with your own search
-        #       method by combining techniques from lecture
-        #
-        # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
-        import random
-        self.queue.put(random.choice(state.actions()))
+        # randomly select a move as player 1 or 2 on an empty board, otherwise
+        # return the optimal minimax move at a fixed search depth of 3 plies
+        if state.ply_count < 2:
+            self.queue.put(random.choice(state.actions()))
+        else:
+            self.queue.put(self.minimax(state, depth=3))
 
-        def _minimax(self, state, depth):
+    def minimax(self, state, depth):
 
-            def _min_value(state, depth):
-                if state.terminal_test(): return state.utility(self.player_id)
-                if depth <= 0: return self.score(state)
-                value = float("inf")
-                for action in state.actions():
-                    value = min(value, _max_value(state.result(action), depth - 1))
-                return value
+        def min_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1))
+            return value
 
-            def _max_value(state, depth):
-                if state.terminal_test(): return state.utility(self.player_id)
-                if depth <= 0: return self.score(state)
-                value = float("-inf")
-                for action in state.actions():
-                    value = max(value, _min_value(state.result(action), depth - 1))
-                return value
+        def max_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1))
+            return value
 
-            return max(state.actions(), key=lambda x: _min_value(state.result(x), depth - 1))
+        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1))
+
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        return len(own_liberties) - len(opp_liberties)
